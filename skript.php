8393d6383fe0e0c2c70a920e937909a856c9b324
Patk00 <marecp700@gmail.com>
2026-04-16 11:07:51 +0200

    prepojenie databazy

--- skript.php
+++ skript.php
@@ -6,6 +6,18 @@
     <title>Projekt</title>
 </head>
 <body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "filmova_aplikacia";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Chyba pripojenia: " . $conn->connect_error);
+}
+?>
     
 </body>
 </html>
